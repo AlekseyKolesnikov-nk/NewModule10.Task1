f56d52d94dca376851b76367e2816dbfc5ce10f4
AlekseyKolesnikov-nk <Aleksey.Kolesnikov@evraz.com>
2024-10-22 22:45:48 +0700

    Добавьте файлы проекта.

---
+++ DataEnter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewModule10.Task1
+{
+    internal class DataEnter
+    {
+        public static void DEnter()
+        {
+            Console.WriteLine("Введите знак сложения - '+'");
+            char oper = Convert.ToChar(Console.ReadLine());
+            if (oper != '+') throw new Exception("Ошибка ввода");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Net.Mail;
+
+namespace NewModule10.Task1
+{
+     interface ICalculator                   
+    {
+        double NumberOne();
+        double NumberTwo();
+        double Result();
+    }
+
+    public class Calculator : ICalculator          
+    {
+        double numberOne;
+        double numberTwo;
+        double resultInches;
+
+        Calculator(double numOne, double numTwo, double result)
+        {
+            numberOne = numOne;
+            numberTwo = numTwo;
+            resultInches = result;
+        }
+        double ICalculator.NumberOne()
+        {
+            return numberOne;
+        }
+        double ICalculator.NumberTwo()
+        {
+            return numberTwo;
+        }
+
+        double ICalculator.Result()
+        {
+            return resultInches;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("Введите первое число");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            bool bad = true;
+            while (bad)
+            {
+                try
+                {
+                    DataEnter.DEnter();
+                    bad = false;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка ввода");
+                };
+            }
+
+            Console.WriteLine("Введите второе число");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double summ = x + y;
+
+            Calculator calc = new Calculator(x, y, summ);
+            ICalculator calculator = calc;
+
+            Console.WriteLine("Первое число: {0}", calculator.NumberOne());
+            Console.WriteLine("Второе число: {0}", calculator.NumberTwo());
+            Console.WriteLine("Сумма чисел: {0}", calculator.Result());
+        }
+    }
+}
